c674fa3e16f04f2bbbe6c8cc1c8432938be6b106
Ahmad Zaky W <azakyw@gmail.com>
2023-10-04 16:21:27 +0700

    Initial Commit

---
+++ BitHealthProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BitHealthProject
+//
+//  Created by Ahmad Zaky W on 04/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
