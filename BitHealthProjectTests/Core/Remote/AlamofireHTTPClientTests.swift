c0b5be520064d3b20ce553d0808bb747187e915c
Ahmad Zaky W <azakyw@gmail.com>
2023-10-05 12:30:00 +0700

    AlamofireHTTPClient.performRequest fails on all invalid representationcases

--- BitHealthProjectTests/Core/Remote/AlamofireHTTPClientTests.swift
+++ BitHealthProjectTests/Core/Remote/AlamofireHTTPClientTests.swift
@@ -40,6 +40,18 @@ final class AlamofireHTTPClientTests: XCTestCase {
         
         XCTAssertNotNil(receivedError)
     }
+    
+    func test_performRequest_failsOnAllInvalidRepresentationCases() {
+        XCTAssertNotNil(resultErrorFor((data: nil, response: nil, error: nil)))
+        XCTAssertNotNil(resultErrorFor((data: nil, response: nonHTTPURLResponse(), error: nil)))
+        XCTAssertNotNil(resultErrorFor((data: anyData(), response: nil, error: nil)))
+        XCTAssertNotNil(resultErrorFor((data: anyData(), response: nil, error: anyNSError())))
+        XCTAssertNotNil(resultErrorFor((data: nil, response: nonHTTPURLResponse(), error: anyNSError())))
+        XCTAssertNotNil(resultErrorFor((data: nil, response: anyHTTPURLResponse(), error: anyNSError())))
+        XCTAssertNotNil(resultErrorFor((data: anyData(), response: nonHTTPURLResponse(), error: anyNSError())))
+        XCTAssertNotNil(resultErrorFor((data: anyData(), response: anyHTTPURLResponse(), error: anyNSError())))
+        XCTAssertNotNil(resultErrorFor((data: anyData(), response: nonHTTPURLResponse(), error: nil)))
+    }
 
     // MARK: - Helpers
     
