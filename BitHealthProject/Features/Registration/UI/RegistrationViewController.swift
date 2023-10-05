3d4c295c6b987d05e8ee28dcb4570197e8b4cabe
Ahmad Zaky W <azakyw@gmail.com>
2023-10-05 10:59:02 +0700

    Create Registration VC UI

--- BitHealthProject/Features/Registration/UI/RegistrationViewController.swift
+++ BitHealthProject/Features/Registration/UI/RegistrationViewController.swift
@@ -5,4 +5,92 @@
 //  Created by Ahmad Zaky W on 04/10/23.
 //
 
-import Foundation
+import UIKit
+
+final class RegistrationViewController: UIViewController {
+    
+    private let stackView: UIStackView = {
+        let view = UIStackView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.axis = .vertical
+        view.spacing = 18
+        return view
+    }()
+    
+    private let icon: UIImageView = {
+        let view = UIImageView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.contentMode = .scaleAspectFit
+        view.image = .tmdbIcon
+        view.setCornerRadius(8)
+        return view
+    }()
+    
+    private let usernameField: UITextField = {
+        let view = UITextField()
+        view.setCornerRadius(8)
+        view.addBorder()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.setLeftPaddingPoints(8)
+        view.setRightPaddingPoints(8)
+        view.placeholder = "Input your username"
+        return view
+    }()
+    
+    private let passwordField: UITextField = {
+        let view = UITextField()
+        view.setCornerRadius(8)
+        view.addBorder()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.setLeftPaddingPoints(8)
+        view.setRightPaddingPoints(8)
+        view.isSecureTextEntry = true
+        view.placeholder = "Input your password"
+        return view
+    }()
+    
+    private lazy var signUpButton: UIButton = {
+        let view = UIButton()
+        view.addTarget(self, action: #selector(didTapSignUp), for: .touchUpInside)
+        view.setTitle("Login", for: .normal)
+        view.backgroundColor = .systemBlue
+        view.setCornerRadius(8)
+        return view
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        title = "Registration"
+        setupView()
+    }
+    
+}
+
+extension RegistrationViewController {
+    private func setupView() {
+        [icon, stackView].forEach { view.addSubview($0) }
+        [usernameField, passwordField, signUpButton].forEach { stackView.addArrangedSubview($0) }
+        
+        NSLayoutConstraint.activate([
+            icon.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            icon.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
+            icon.widthAnchor.constraint(equalToConstant: 100),
+            icon.heightAnchor.constraint(equalToConstant: 100),
+            
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            
+            usernameField.heightAnchor.constraint(equalToConstant: 44),
+            passwordField.heightAnchor.constraint(equalToConstant: 44),
+            signUpButton.heightAnchor.constraint(equalToConstant: 44),
+        ])
+    }
+    
+    @objc
+    private func didTapSignUp() {
+        print("go to login page")
+    }
+}
+
